5619962599499c4ca45c48b1d4047611ff418303
Sxmmie <samuelumohjnr@gmail..com>
2020-07-14 22:34:41 +0100

    Writing tests for the VotingPollFactory

---
+++ VotingApp.Tests/VotingPollFactoryTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace VotingApp.Tests
+{
+    public class VotingPollFactoryTests
+    {
+        private VotingPollFactory _factory = new VotingPollFactory();
+        private string[] _names = new[] { "name1", "names2" };
+        private string _title = "title";
+        private string _description = "description";
+
+        [Fact]
+        public void Create_ThrowsWhenLessThanTwoCounterNames()
+        {
+            Throws<ArgumentException>(() => _factory.Create(_title, _description, new[] { "name" }));
+            Throws<ArgumentException>(() => _factory.Create(_title, _description, new string[] { }));
+        }
+
+        [Fact]
+        public void Create_AddsCounterToThePollForEachName()
+        {
+            var poll = _factory.Create(_title, _description, _names);
+
+            foreach(var names in names)
+            {
+
+            }
+        }
+    }
+}
